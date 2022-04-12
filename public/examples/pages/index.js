0a28eadd1463e89d729033043a159c369c6ef1a2
霍晓亮 <18713204353@163.com>
2022-04-12 15:01:34 +0800

    基础地图改为配置方式

--- public/examples/pages/index.js
+++ public/examples/pages/index.js
@@ -1,15 +1,114 @@
 /*
  * @Author: hxl
  * @Date: 2021-08-16 14:58:53
- * @LastEditTime: 2022-04-06 17:38:52
+ * @LastEditTime: 2022-04-12 14:35:46
  * @LastEditors: hxl
  * @Description:
  */
 var map = undefined
 var layer = undefined
-
+const baseMap = [
+  {
+    groupId: 10,
+    id: 2000,
+    name: '天地图影像',
+    iconUrl: 'sdk/images/icon/天地图.png',
+    layer: [
+      {
+        name: '底图',
+        type: 'tdt',
+        mapStyle: 'img_w'
+      },
+      {
+        name: '注记',
+        type: 'tdt',
+        mapStyle: 'cia_w'
+      }
+    ]
+  },
+  {
+    groupId: 10,
+    id: 2001,
+    name: '天地图电子',
+    iconUrl: 'sdk/images/icon/天地图电子.png',
+    layer: [
+      {
+        name: '底图',
+        type: 'tdt',
+        mapStyle: 'vec_w'
+      },
+      {
+        name: '注记',
+        type: 'tdt',
+        mapStyle: 'cva_w'
+      }
+    ]
+  },
+  {
+    groupId: 10,
+    id: 2002,
+    name: '百度影像',
+    iconUrl: 'sdk/images/icon/百度.png',
+    layer: [
+      {
+        name: '底图',
+        type: 'baidu',
+        style: 'img'
+      },
+      {
+        name: '注记',
+        type: 'baidu',
+        style: 'vec'
+      }
+    ]
+  },
+  {
+    groupId: 10,
+    id: 2003,
+    name: 'arcgis影像',
+    iconUrl: 'sdk/images/icon/arcgis.png',
+    layer: [
+      {
+        name: '底图',
+        type: 'arcgis',
+        url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer'
+      }
+    ]
+  },
+  {
+    groupId: 10,
+    id: 2004,
+    name: 'mapbox',
+    iconUrl: 'sdk/images/icon/mapbox.png',
+    show: true,
+    layer: [
+      {
+        type: 'mapbox_style',
+        url: 'https://api.mapbox.com/styles/v1',
+        username: 'huoxiaoliang',
+        styleId: 'ckfdljnsa0d9y19sciu9cr4zb',
+        accessToken: 'pk.eyJ1IjoiaHVveGlhb2xpYW5nIiwiYSI6ImNrZmRld290NjA1dmIycnQ3cjdsZGJ0ZzUifQ.Tj-V7Lr23YZSduFkdKS8nA',
+        scaleFactor: true,
+        credit: 'mapbox'
+      }
+    ]
+  },
+  {
+    groupId: 10,
+    id: 2005,
+    name: '蓝色底图',
+    iconUrl: 'sdk/images/icon/蓝色底图.png',
+    show: true,
+    layer: [
+      {
+        type: 'xyz',
+        url: 'http://map.geoq.cn/arcgis/rest/services/ChinaOnlineStreetPurplishBlue/MapServer/tile/{z}/{y}/{x}'
+      }
+    ]
+  }
+]
 function initViewer() {
-  map = new Creatar3d.Viewer('viewer-container')
+  map = new Creatar3d.Viewer('viewer-container', { baseMap })
   map.setOptions({
     globe: {
       depthTestAgainstTerrain: true
